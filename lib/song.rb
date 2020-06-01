5f0cc52c753a87e225df60fc593424a3b3ae7395
Billy Ferrara <willfira717@gmail.com>
2020-06-01 19:50:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,43 @@
-# class Song
-#   attr_accessor :artist, :name, :genre
-#
-#   @@all = []
-#
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def artist_name
-#     artist.name
-#   end
-#
-#   def save
-#     @@all << self
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-# end
-
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :artist, :name, :genre
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
+    artist.name
   end
 
-end 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
+# class Song
+#   attr_accessor :artist, :name
+# 
+#   def initialize(name)
+#     @name = name
+#   end
+# 
+#   def artist_name
+#     if artist
+#       self.artist.name
+#     else
+#       nil
+#     end
+#   end
+# 
+# end 
