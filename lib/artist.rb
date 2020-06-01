1edff0bbd2ab6b0a0e6b77a08bacd259504fa8e2
Billy Ferrara <willfira717@gmail.com>
2020-06-01 19:48:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,60 @@
-class Artist
-  
-  attr_accessor :name
+# class Artist
+#
+#   attr_accessor :name
+#
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+#
+#   def songs
+#     Song.all.select {|song| song.artist == self}
+#   end
+#
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
+#
+#   def add_song_by_name(song_name)
+#   singer = Song.new(song_name)
+#     add_song(singer)
+#   end
+#
+#   def self.song_count
+#     Song.all.count
+#   end
+#
+#
+# end
 
+class Artist
+  attr_accessor :name
+  @@song_count = 0
   def initialize(name)
     @name = name
-    @songs = []
+    @songs = [] #empty collection of songs
   end
 
   def songs
-    Song.all.select {|song| song.artist == self}
+    @songs
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(song_name)
-  singer = Song.new(song_name)
-    add_song(singer)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-    Song.all.count
+    @@song_count
   end
 
-
-end
+end 
