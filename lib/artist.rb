ecd9cf4684bca3ab4ffc2e02b616828ec0cc9dfb
Billy Ferrara <willfira717@gmail.com>
2020-06-01 19:50:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -35,26 +35,26 @@ end
 #     @name = name
 #     @songs = [] #empty collection of songs
 #   end
-# 
+#
 #   def songs
 #     @songs
 #   end
-# 
+#
 #   def add_song(song)
 #     @songs << song
 #     song.artist = self
 #     @@song_count += 1
 #   end
-# 
+#
 #   def add_song_by_name(song_name)
 #     song = Song.new(song_name)
 #     @songs << song
 #     song.artist = self
 #     @@song_count += 1
 #   end
-# 
+#
 #   def self.song_count
 #     @@song_count
 #   end
-# 
-# end 
+#
+# end
