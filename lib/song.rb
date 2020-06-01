456a0e204c272b0b86cc83e0b32b3f48616042df
Billy Ferrara <willfira717@gmail.com>
2020-06-01 19:51:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,11 +27,11 @@ end
 
 # class Song
 #   attr_accessor :artist, :name
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
-# 
+#
 #   def artist_name
 #     if artist
 #       self.artist.name
@@ -39,5 +39,5 @@ end
 #       nil
 #     end
 #   end
-# 
-# end 
+#
+# end
